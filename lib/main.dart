46be01c2ebf6b4a7c7818c65a9d588a94e3852a5
101230308 <101230308@mu.edu.lb>
2026-05-12 00:03:36 +0300

    Add localization and update home screen to Register
    Updated the main application to include new screens and localization support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:autism_world/screens/childPage.dart';
 import 'package:autism_world/screens/login.dart';
-import 'package:autism_world/screens/specialist.dart';
+import 'package:autism_world/screens/register.dart';
+import 'package:autism_world/specialist/specialist.dart';
+import 'package:autism_world/screens/volunteer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:autism_world/l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,9 +17,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SpecialistPage(),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('en', 'US'), Locale('ar', 'SA')],
+      locale: const Locale('ar', 'SA'), // Set Arabic as the default locale
+      home: Register(),
     );
   }
 }
+
