fcc495d5742dd5bd8cfbf67602be9ba1b15d50e8
101230308 <101230308@mu.edu.lb>
2026-07-02 03:11:30 +0300

    Implement authentication storage methods
    Added methods for saving and loading authentication data.

--- lib/screens/shared_prefernces.dart
+++ lib/screens/shared_prefernces.dart
@@ -3,7 +3,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SettingsStorage {
   static const _darkKey = "darkMode";
   static const _langKey = "language";
-  static const _notifKey = "notifications";
+
+  // Authentication Storage Keys
+  static const _tokenKey = "auth_token";
+  static const _userIdKey = "user_id";
 
   static Future<void> saveDarkMode(bool value) async {
     final prefs = await SharedPreferences.getInstance();
@@ -25,13 +28,31 @@ class SettingsStorage {
     return prefs.getString(_langKey) ?? "en";
   }
 
-  static Future<void> saveNotifications(bool value) async {
+  // --- AUTHENTICATION STORAGE HELPER METHODS ---
+
+  static Future<void> saveAuthToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(_notifKey, value);
+    await prefs.setString(_tokenKey, token);
   }
 
-  static Future<bool> loadNotifications() async {
+  static Future<String?> loadAuthToken() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(_notifKey) ?? true;
+    return prefs.getString(_tokenKey);
+  }
+
+  static Future<void> saveUserId(int id) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_userIdKey, id);
+  }
+
+  static Future<int?> loadUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_userIdKey);
+  }
+
+  static Future<void> clearAuthData() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_tokenKey);
+    await prefs.remove(_userIdKey);
   }
 }
